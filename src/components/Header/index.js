ae7ee1afe17351dcc43fc80440e4e30ccf31493d
Matheus Pavanelli <mspavanelli@gmail.com>
2019-11-30 17:06:23 -0300

    Add App and Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const Header = () => <Container>Hey</Container>;
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  align-items: center;
+  background: #4a90e2;
+  display: flex;
+  height: 64px;
+  justify-content: space-between;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
+`;
